bf086e58c5cb0ad2bdc12961e9df486acb85de5d
panda3123-lgtm <panda3.123@icloud.com>
2026-07-11 09:44:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,209 @@
-let cards = [];
+let allCards = [];
+
+let deck = [];
 
 let hand = [];
 
+let field = [];
+
+let grave = [];
+
+let playerLP = 8000;
+
 let cost = 1;
+
+let turn = 1;
+
 
 
 // cards.json読み込み
 
 fetch("cards.json")
+
 .then(response => response.json())
+
 .then(data => {
 
-    cards = data;
 
-    // 仮の初期手札5枚
-    hand = cards.slice(0,5);
+    allCards = data;
 
-    showHand();
+
+    startGame();
+
 
 });
 
 
 
+
+// ゲーム開始
+
+function startGame(){
+
+
+    deck = [...allCards];
+
+
+    shuffle(deck);
+
+
+
+    // 初期手札5枚
+
+    for(let i = 0; i < 5; i++){
+
+        draw();
+
+    }
+
+
+    update();
+
+
+}
+
+
+
+
+// シャッフル
+
+function shuffle(array){
+
+
+    for(let i=array.length-1;i>0;i--){
+
+
+        let j=Math.floor(Math.random()*(i+1));
+
+
+        [array[i],array[j]]=[array[j],array[i]];
+
+
+    }
+
+}
+
+
+
+
+// ドロー
+
+function draw(){
+
+
+    if(deck.length===0){
+
+        alert("デッキ切れ！敗北");
+
+        return;
+
+    }
+
+
+    hand.push(deck.shift());
+
+
+}
+
+
+
+
 // 手札表示
 
-function showHand(){
+function update(){
 
-    const handDiv = document.getElementById("hand");
 
-    handDiv.innerHTML = "";
+    document.getElementById("playerLP").innerText = playerLP;
+
+    document.getElementById("cost").innerText = cost;
+
+
+
+    let handDiv=document.getElementById("hand");
+
+    handDiv.innerHTML="";
+
 
 
     hand.forEach((card,index)=>{
 
 
-        const div = document.createElement("div");
-
-        div.className = "card";
+        let div=document.createElement("div");
 
 
-        div.innerHTML = `
+        div.className="card";
+
+
+        div.innerHTML=`
+
         <b>${card.name}</b>
+
         <br>
+
         コスト:${card.cost}
+
         <br>
+
         ATK:${card.atk}
+
         `;
 
 
-        div.onclick = ()=>{
 
-            playCard(index);
+        div.onclick=()=>playCard(index);
 
-        };
 
 
         handDiv.appendChild(div);
 
 
+
     });
 
+
+
+    let fieldDiv=document.getElementById("player-field");
+
+    fieldDiv.innerHTML="自分モンスターエリア";
+
+
+
+    field.forEach(card=>{
+
+
+        let div=document.createElement("div");
+
+
+        div.className="card";
+
+
+        div.innerHTML=`
+
+        <b>${card.name}</b>
+
+        <br>
+
+        ATK:${card.atk}
+
+        `;
+
+
+
+        div.onclick=()=>attack(card);
+
+
+
+        fieldDiv.appendChild(div);
+
+
+
+    });
+
+
+
 }
+
+
 
 
 
@@ -69,72 +212,117 @@ function showHand(){
 function playCard(index){
 
 
-    const card = hand[index];
+    let card=hand[index];
+
 
 
     if(card.cost > cost){
 
-        alert("コスト不足！");
+
+        alert("コスト不足");
 
         return;
+
 
     }
 
 
-
-    // コスト消費
 
     cost -= card.cost;
 
 
-    document.getElementById("cost").innerText = cost;
-
-
-
-    // モンスターなら召喚
-
-    if(card.type === "monster"){
-
-        summon(card);
-
-    }
-
-
-    // 手札から削除
 
     hand.splice(index,1);
 
 
-    showHand();
+
+    if(card.type==="monster"){
+
+
+        field.push(card);
+
+
+    }
+
+
+    else{
+
+
+        alert(card.name+"を発動しました");
+
+
+        grave.push(card);
+
+
+    }
+
+
+
+    update();
+
 
 }
 
 
 
-// 召喚
-
-function summon(card){
 
 
-    const field =
-    document.getElementById("player-field");
+// 攻撃
+
+function attack(card){
 
 
-    const div =
-    document.createElement("div");
+    let damage = card.atk;
 
 
-    div.className="card";
+    alert(
+
+    card.name+
+
+    "で攻撃！\n"+
+
+    damage+
+
+    "ダメージ"
+
+    );
 
 
-    div.innerHTML=
-    `
-    <b>${card.name}</b>
-    <br>
-    ATK:${card.atk}
-    `;
+}
 
 
-    field.appendChild(div);
+
+
+
+// ターン終了
+
+function endTurn(){
+
+
+    turn++;
+
+
+
+    // コスト回復
+
+    cost += 2;
+
+
+    if(cost>10){
+
+        cost=10;
+
+    }
+
+
+
+    // ドロー
+
+    draw();
+
+
+
+    update();
+
 
 }
